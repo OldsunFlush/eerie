2db5d18c9908725ad9c9ad6db761bb46c44baafb
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2024-07-01 21:30:35 -0400

    Update import order

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eerie.pierreprezelin.com",
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
