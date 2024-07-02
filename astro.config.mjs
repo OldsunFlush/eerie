2097cb08a96a37e427a484a84e62ec6872958106
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2024-07-01 21:35:11 -0400

    Removed mdx from packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eerie.pierreprezelin.com",
-  integrations: [mdx(), sitemap(), tailwind()],
-  markdown: {
-    shikiConfig: {
-      theme: "css-variables",
-    },
-  },
+  integrations: [sitemap(), tailwind()],
 });
