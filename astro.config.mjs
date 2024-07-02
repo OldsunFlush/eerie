b01267e44c277d0baed7e79bad99c3b5695471ce
Pierre Prézelin <pierreprezelin.pro@gmail.com>
2024-07-01 21:29:54 -0400

    Update site path in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-micro.vercel.app",
+  site: "https://eerie.pierreprezelin.com",
   integrations: [tailwind(), sitemap(), mdx()],
   markdown: {
     shikiConfig: {
